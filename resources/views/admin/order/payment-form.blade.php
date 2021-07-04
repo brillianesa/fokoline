caa6caaf627c3dedaf5421add7548e3102b58fe2
rifqicode <muhammadrifqi.tb@gmail.com>
2021-07-04 09:06:51 +0700

    update: small detail

--- resources/views/admin/order/payment-form.blade.php
+++ resources/views/admin/order/payment-form.blade.php
@@ -41,7 +41,7 @@
                                 <div class="form-group">
                                     <div class="col-sm-12">
                                         <button type="submit" class="btn btn-primary pull-right" style="margin-left: 5px;"> Upload </button>
-                                        <a class="btn btn-danger pull-right"> Cancel </a>
+                                        <a href="{{ route('order.list') }}" class="btn btn-danger pull-right"> Batalkan </a>
                                     </div>
                                 </div>
                             </div>
